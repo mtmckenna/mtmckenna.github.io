4266690911d1a2c75acf9453042b2478d9032204
Matt McKenna <matt@mtmckenna.com>
2013-01-05 20:05:44 -0800

    This is something like an init commit. Totally redid site.

--- inc/js/mtmckenna.js
+++
@@ -1,8 +0,0 @@
-function contentResize() {
-    if ($(window).width() <= 500) {
-        document.getElementById('content').style.width = ($(window).width() - 30 ) + 'px';
-    }
-    else {
-        document.getElementById('content').style.width = '500px';
-    }
-}
