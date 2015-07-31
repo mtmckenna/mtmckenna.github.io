587e07ea7bf57f38982a4ba3cfd8d6214a568581
Matt McKenna <matt@mtmckenna.com>
2015-07-31 09:47:23 -0700

    update _s3sync

--- _s3sync.sh
+++ _s3sync.sh
@@ -1,1 +1,2 @@
-s3cmd sync --recursive --acl-public dist/ s3://www.mtmckenna.com/
+#s3cmd sync --recursive --acl-public dist/ s3://www.mtmckenna.com/
+aws s3 sync --profile mtmckenna --region us-east-1  dist/ s3://www.mtmckenna.com/
